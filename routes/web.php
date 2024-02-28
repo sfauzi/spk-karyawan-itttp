8c8f205faf4bb0b0f31a0def82422fb82737ab70
sfauzii <sjahrulfauzii@gmail.com>
2024-02-28 12:19:25 +0700

    fix(routes): error fix routes admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AlternativeController;
+use App\Http\Controllers\Admin\CriteriaRatingController;
+use App\Http\Controllers\Admin\CriteriaWeightController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DecisionController;
+use App\Http\Controllers\Admin\NormalizationController;
+use App\Http\Controllers\Admin\RankController;
 use App\Http\Controllers\HomeController;
+use App\Models\Alternative;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,8 +27,20 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 // Route::get('/admin', [DashboardController::class, 'index'])
 //     ->name('dashboard');
 
+Route::get('decision', [DecisionController::class, 'index']);
+
+Route::get('normalization', [NormalizationController::class, 'index']);
+
+Route::get('rank', [RankController::class, 'index']);
+
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin', [DashboardController::class, 'index'])->name('home');
+    Route::get('/admin', [DashboardController::class, 'index'])->name('dashboard');      
+
+    Route::resources([
+        'alternatives' => AlternativeController::class,
+        'criteriaratings' => CriteriaRatingController::class,
+        'criteriaweights' => CriteriaWeightController::class
+        ]);
 });
 
 // Auth::routes();
