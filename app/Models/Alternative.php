1c99a601e17681b8520316accbe03c3ac5a0ca22
sfauzii <sjahrulfauzii@gmail.com>
2024-02-28 12:18:07 +0700

    fix(error fix): alternative model

--- app/Models/Alternative.php
+++ app/Models/Alternative.php
@@ -9,9 +9,24 @@ class Alternative extends Model
 {
     use HasFactory;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+
     protected $table = 'alternatives';
 
+    
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name'
     ];
+
+   
 }
